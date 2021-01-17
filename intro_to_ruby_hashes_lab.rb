5210a4f3c0944c1ba8eb4c1be57831526e580eb5
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-17 19:00:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,13 +12,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly =  {
-  :railroads =>
-  {
-    :pieces => 4
+onopoly = {
+    :railroads => 
+    {
+      :pieces => 4 
+    }
   }
-}
-
 end
 
 def monopoly_with_third_tier
